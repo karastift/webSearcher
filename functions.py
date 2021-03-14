2dbc8bca5f1ab3b1343027728ba88a3b16e69b8e
Kian van der Meer <kiansinan@gmail.com>
2021-03-14 11:18:11 +0100

    First release

---
+++ functions.py
@@ -0,0 +1,56 @@
+import requests as re
+import random as ra
+from bs4 import BeautifulSoup
+
+class Functions():
+    OKBLUE = '\033[94m'
+    FAIL = '\033[91m'
+    OKCYAN = '\033[96m'
+    ENDC = '\033[0m'
+
+    def validateArgs(self, amount, request, topleveldomain):
+        failed = False
+        if  isinstance(amount, int) != True:
+            print(f'{self.FAIL}First argument has to be an integer!')
+            failed = True
+        if request != 'http://' and request != 'https://':
+            print(f'{self.FAIL}Second argument has to be \'http://\' or \'https://\'! It was \'{request}\'.')
+            failed = True
+        if topleveldomain[0] != '.':
+            print(f'{self.FAIL}Third argument has to have the \'.\' before your topleveldomain!')
+            failed = True
+        if failed:
+            return True
+        else:
+            return False
+
+    def generateUrl(self, request, topleveldomain):
+        numbers = []
+        numbers.append(str(ra.randint(0,9999)))
+        url = str(request) + (''.join(numbers)) + str(topleveldomain)
+        info = []
+        info.append(url)
+        url = ''
+        return info
+
+    def requestContent(self, info):
+        r = re.get(info[0])
+        info.append(r.content)
+        return info
+
+    def parseContent(self, info):
+        soup = BeautifulSoup(info[1], 'html.parser')
+        info.append(soup.title.string)
+        return info
+
+    def writeToFile(self, info):
+            if ('sale' in info[2]) == True:
+                    f.write((info[0] + ' [SALE] \n'))
+            elif ('Forbidden' in info[2]) == True:
+                with open('/Users/kara/Coding/python/websiteSearcher/output.txt', 'a') as f:
+                    f.write((info[0] + ' [FORBIDDEN] \n'))
+            else:
+                with open('/Users/kara/Coding/python/websiteSearcher/output.txt', 'a') as f:
+                    f.write((info[0] + ' --> '))
+                    f.write((info[2] + '\n'))
+            f.close()
---
+++ main.py
@@ -0,0 +1,45 @@
+import functions
+import argparse
+
+def main():
+    OKBLUE = '\033[94m'
+    FAIL = '\033[91m'
+    OKCYAN = '\033[96m'
+    ENDC = '\033[0m'
+
+    parser = argparse.ArgumentParser(description='check urls.')
+
+    parser.add_argument('amount', metavar='amount', type=int,
+                    help='an integer for the amount of domains to be checked')
+
+    parser.add_argument('request', metavar='request', type=str,
+                    help='a string that that says http:// or https://')
+    
+    parser.add_argument('topleveldomain', metavar='topleveldomain', type=str,
+                    help='a string that that indicates the toplevelodomain: .com / .us ...')
+
+    args = parser.parse_args()
+    amount = args.amount
+    request = args.request
+    topleveldomain = args.topleveldomain
+
+    fun = functions.Functions()
+
+    if fun.validateArgs(amount, request, topleveldomain) == False:
+        r = request
+        t = topleveldomain
+        a = amount
+        for i in range(1, a+1, 1):
+            print(f'{OKBLUE}{i}')
+            url = fun.generateUrl(r, t)
+            try:
+                request = fun.requestContent(url)
+                print(f'{OKCYAN}{url[0]}')
+                parser = fun.parseContent(request)
+                fun.writeToFile(parser)
+            except:
+                print(f'{FAIL}{url[0]}')
+        print(f'{ENDC}finished checking {a} domains.')
+
+if __name__ == '__main__':
+    main()
